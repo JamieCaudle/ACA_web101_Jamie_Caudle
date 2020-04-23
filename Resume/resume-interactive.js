b301db9f4e53ba6bf15bfb5a4db543515e2c2fe7
JamieCaudle <jamielcaudle@gmail.com>
2020-04-23 15:51:02 -0500

    ready for turn in

---
+++ Resume/resume-interactive.js
@@ -0,0 +1,9 @@
+const hidePic = () => { document.getElementById("profilePic").style.visibility = "hidden"; }
+
+const fadeIn = () => {
+document.getElementById('name').classList.add('animated', 'fadeIn', 'slower');
+}
+
+const showNumber = () => { 
+document.getElementById("number").style.color = "white";
+}
