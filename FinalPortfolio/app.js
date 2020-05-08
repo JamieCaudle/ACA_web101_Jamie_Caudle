2c4265b54d3b21cdc9661df5cbf1326cff6ef2d4
JamieCaudle <jamielcaudle@gmail.com>
2020-05-08 15:43:47 -0500

    day one changes

---
+++ FinalPortfolio/app.js
@@ -0,0 +1,18 @@
+// let x = window.matchMedia('(min-width: 992px)');
+//   if (x.matches) {
+//     document.body.style.backgroundColor = "yellow";
+//   } 
+//   else {
+//     document.body.style.backgroundColor = "pink";
+//   }
+
+    
+// const removeAnimation = () => {
+//    let animation = document.getElementById('textContainer');
+//    animation.classList.remove('animate__lightSpeedInLeft')
+// }
+
+// const addAnimation = () => {
+//   let addAnimation = document.getElementById('textContainer');
+//   addAnimation.classList.add("animate__slideInUp");
+// }
